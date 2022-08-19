9c30128a8a83dc4748081ffe3c68b90bde3086cf
Chiragsharma <chiragsharma250999@gmail.com>
2022-08-20 05:23:10 +0530

    Done!

---
+++ axios-crash-master/headers.js
@@ -0,0 +1,23 @@
+/*
+
+what are headers in network call..?
+    headers are used for sending things which you require to send everytime.
+    USE: we need to send a special key to get verified and connect to something (let's say facebook chat with someone.)
+use of axios: if you want to do smthg when you make a request from Front end to Backend like: 
+axios.post(url, { }) 
+eg => contenttype: application/json
+
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
+
+Axios is a tool to communicate from front end to the backend. it is used for network calls.
+
+backend is a robot listening to the above port.
+url => baseurl(including http)/route
+baseurl => flipcart.com
+Route - adding /getproducts specifies where to go on this domain. => execute some code bts.
+
+
+
+
+*/
